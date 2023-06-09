96275b094194b4a73bdad02d724e9ca8b19e4765
Asad Tanvir Shami <91515695+asadtanvirshami@users.noreply.github.com>
2023-06-09 21:43:58 +0500

    bubbleSort implementation

---
+++ bubbleSort.cpp
@@ -0,0 +1,36 @@
+// Bubble sort in C++
+#include <iostream>
+using namespace std;
+
+// perform bubble sort
+void bubbleSort(int array[], int size) {
+
+for(int step = 0; step < size; ++step){
+	
+	for(int i = 0; i < size - step; ++i){
+		if(array[i] > array[i+1]){
+			int temp = array[i];
+			array[i] = array[i+1];
+			array[i+1]=temp;
+		}
+	}
+  }
+}
+
+void printArray(int array[], int size){
+	for(int i = 0; i < size; ++i){
+		cout<<" "<< array[i];
+	}
+	cout << "\n";
+}
+
+int main (){
+	int data[] = {-2,3,13,15,5,9};
+	int size = sizeof(data)/sizeof(data[0]);
+	
+	bubbleSort(data,size);
+	cout << "Array is: ";
+	
+	printArray(data,size);
+
+}

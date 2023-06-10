0bb7d10640d027f95e5e93fd153ee6c753cfad92
Asad Tanvir Shami <91515695+asadtanvirshami@users.noreply.github.com>
2023-06-10 07:26:56 +0500

    binary search implementation

---
+++ binarySearch.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+
+int binarySearch(int arr[], int left, int right, int target) {
+    while (left <= right) {
+        int mid = left + (right - left) / 2;
+        if (arr[mid] == target) {
+            return mid;  // Return the index if the target is found
+        } else if (arr[mid] < target) {
+            left = mid + 1;
+        } else {
+            right = mid - 1;
+        }
+    }
+    return -1;  // Return -1 if the target is not found
+}
+
+int main() {
+    int arr[] = {1, 2, 3, 4, 5};
+    int size = sizeof(arr) / sizeof(arr[0]);
+    int target = 3;
+
+    int index = binarySearch(arr, 0, size - 1, target);
+    if (index != -1) {
+        std::cout << "Element found at index " << index << std::endl;
+    } else {
+        std::cout << "Element not found" << std::endl;
+    }
+
+    return 0;
+}
